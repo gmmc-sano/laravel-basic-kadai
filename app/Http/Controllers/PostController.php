9497c9023cad878fe4b958e519aa483218d7cd8b
gmmc-sano <gmmc-sano@gmail.com>
2024-03-10 01:50:06 +0900

    fourth

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,4 +23,25 @@ class PostController extends Controller
         // 変数$postをpost/show.blade.phpに渡す
         return view('posts.show',compact('post'));
     }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        // バリデーションを設定
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // フォームの入力内容をもとに、テーブルにデータを追加
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        // リダイレクト
+        return redirect("/posts");
+    }
 }
